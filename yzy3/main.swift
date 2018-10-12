b5dc6d6f1ab00920028d2602d32bf5727895afb3
ciec6575 <1191084066@qq.com>
2018-10-12 19:36:43 +0800

    yzy3
    排序

--- yzy3/main.swift
+++ yzy3/main.swift
@@ -7,6 +7,21 @@
 //
 
 import Foundation
-
-print("Hello, World!")
+var a:[Int]=[18,23,5,48,12,36,190,56,99,10]
+for i in 0..<a.count
+{
+    for j in 0..<a.count - (i+1)
+    {
+        if a[j]>a[j+1]
+        {
+            let temp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
+        }
+    }
+}
+for i in 0..<a.count
+{
+    print(a[i])
+}
 
